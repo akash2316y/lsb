5c0baf11ba90ea0a5005bedcb3ed4d1d4c4dd4ce
akash2316y <iamakashrabha@gmail.com>
2026-08-24 10:32:25 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,8 +2,9 @@ from aiohttp import web
 from .route import routes
 
 
-async def web_server():
+async def web_server(bot=None):
     web_app = web.Application(client_max_size=30000000)
+    web_app["bot"] = bot
     web_app.add_routes(routes)
     return web_app
-# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
+
