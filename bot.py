062e18d5ebc7fd8ab1e00f2cec6e85e031224d38
akash2316y <iamakashrabha@gmail.com>
2026-08-24 10:28:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
 import asyncio
 import sys
 from datetime import datetime
@@ -48,12 +49,13 @@ class Bot(Client):
             self.LOGGER(__name__).warning(f"Failed to notify owner ({OWNER_ID}) of bot start: {e}")
 
         self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info("Bot Running..!\n\nCreated by \nhttps://t.me/ProObito")
         self.LOGGER(__name__).info(f"{name}")
         self.username = usr_bot_me.username
 
         # Web-response
         try:
-            app = web.AppRunner(await web_server())
+            app = web.AppRunner(await web_server(self))
             await app.setup()
             bind_address = "0.0.0.0"
             await web.TCPSite(app, bind_address, PORT).start()
